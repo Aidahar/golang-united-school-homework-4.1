e3a81f6a5438cd301ab6488f3c636b7232f29115
Aidahar <zagirik@gmail.com>
2022-05-16 14:28:36 +0300

    solution example 1

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,12 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
+	strLen := len(input) - 1
+	bytStr := []byte(input)
+	var ans []byte
+	for i := strLen; i >= 0; i-- {
+		ans = append(ans, bytStr[i])
+	}
+	output = string(ans)
 	return output
 }
